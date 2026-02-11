750926b6dfb70047ddfc727c59b41e91d8565e33
Vignesh21J <vignesh11212003@gmail.com>
2026-02-11 22:18:25 +0530

    feat: implement elevator movement logic and step-based simulation

---
+++ ElevatorSystem/src/elevator/Main.java
@@ -0,0 +1,60 @@
+package elevator;
+
+import elevator.models.ElevatorCar;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // A Single Elevator
+//        ElevatorCar elevator = new ElevatorCar(1, 0);
+//
+//        elevator.addTarget(4);
+//        elevator.addTarget(2);
+//
+//        for (int i = 0; i < 10; i++) {
+//            elevator.step();
+//            // System.out.println("Current floor: " + elevator.getCurrentFloor());
+//
+//            System.out.println("Floor: " + elevator.getCurrentFloor()
+//                    + " | Direction: " + elevator.getDirection()
+//                    + " | State: " + elevator.getState()
+//            );
+//        }
+
+        System.out.println("===== TEST 1: UP Movement =====");
+        testUpMovement();
+
+        System.out.println("\n===== TEST 2: DOWN Movement =====");
+        testDownMovement();
+    }
+
+    private static void testUpMovement() {
+        ElevatorCar elevator = new ElevatorCar(1, 0);
+
+        elevator.addTarget(6);
+        elevator.addTarget(2);
+
+        for (int i = 0; i < 10; i++) {
+            elevator.step();
+            System.out.println("Floor: " + elevator.getCurrentFloor()
+                    + " | Direction: " + elevator.getDirection()
+                    + " | State: " + elevator.getState()
+            );
+        }
+    }
+
+    private static void testDownMovement() {
+        ElevatorCar elevator = new ElevatorCar(1, 7);
+
+        elevator.addTarget(1);
+        elevator.addTarget(3);
+
+        for (int i = 0; i < 10; i++) {
+            elevator.step();
+            System.out.println("Floor: " + elevator.getCurrentFloor()
+                    + " | Direction: " + elevator.getDirection()
+                    + " | State: " + elevator.getState()
+            );
+        }
+    }
+}
--- ElevatorSystem/src/elevator/models/ElevatorCar.java
+++ ElevatorSystem/src/elevator/models/ElevatorCar.java
@@ -69,4 +69,97 @@ public final class ElevatorCar {
     public FanState getFanState() {
         return fan.getState();
     }
+
+    public void addTarget(int floor) {
+        if (floor == getCurrentFloor()) {
+            return;
+        }
+
+        if (floor > getCurrentFloor()) {
+            upStops.add(floor);
+        } else {
+            downStops.add(floor);
+        }
+    }
+
+    public Integer nextStop() {
+        if (getDirection() == Direction.UP) {
+            if (!upStops.isEmpty()) {
+                return upStops.first();
+            } else if (!downStops.isEmpty()) {
+                direction = Direction.DOWN;
+                return downStops.last();
+            }
+        }
+
+        if (getDirection() == Direction.DOWN) {
+            if (!downStops.isEmpty()) {
+                return downStops.last();
+            } else if (!upStops.isEmpty()) {
+                direction = Direction.UP;
+                return upStops.first();
+            }
+        }
+
+        if (getDirection() == Direction.IDLE) {
+            if (!upStops.isEmpty()) {
+                direction = Direction.UP;
+                return upStops.first();
+            } else if (!downStops.isEmpty()) {
+                direction = Direction.DOWN;
+                return downStops.last();
+            }
+        }
+
+        return null; // no stops in both sets.
+    }
+
+    public void moveOneFloor() {
+        if (direction == Direction.IDLE) {
+            return;
+        }
+
+        if (direction == Direction.UP) {
+            currentFloor++;
+        } else if (direction == Direction.DOWN) {
+            currentFloor--;
+        }
+    }
+
+    public boolean shouldStopAtCurrentFloor() {
+        return upStops.contains(currentFloor) || downStops.contains(currentFloor);
+    }
+
+    public void removeStopAtCurrentFloor() {
+        upStops.remove(currentFloor);
+        downStops.remove(currentFloor);
+    }
+
+    public void step() {
+        Integer target = nextStop();
+
+        if (target == null) {
+            direction = Direction.IDLE;
+            state = ElevatorState.IDLE;
+            return;
+        }
+
+        state = ElevatorState.MOVING;
+
+        moveOneFloor();
+
+        if (shouldStopAtCurrentFloor()) {
+
+            state = ElevatorState.DOOR_OPEN;
+
+            removeStopAtCurrentFloor();
+
+            state = ElevatorState.IDLE;
+
+            // Go idle ONLY if no pending requests
+            if (upStops.isEmpty() && downStops.isEmpty()) {
+                direction = Direction.IDLE;
+            }
+        }
+    }
 }
